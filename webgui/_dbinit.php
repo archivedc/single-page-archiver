5cc84d3870a95b581f461a3051f9460e021f8813
mkaraki <26180919+mkaraki@users.noreply.github.com>
2022-04-23 00:39:04 +0900

    Add unlisted column

--- webgui/_dbinit.php
+++ webgui/_dbinit.php
@@ -4,5 +4,5 @@ require_once 'vendor/autoload.php';
 
 $db = new MeekroDB($dbhost, $dbuser, $dbpass, $dbname, null, 'utf8mb4');
 
-$db->query('CREATE TABLE IF NOT EXISTS history(id INT NOT NULL auto_increment PRIMARY KEY, time INT NOT NULL, url TEXT NOT NULL, title TEXT DEFAULT NULL) DEFAULT CHARSET = utf8mb4');
+$db->query('CREATE TABLE IF NOT EXISTS history(id INT NOT NULL auto_increment PRIMARY KEY, time INT NOT NULL, url TEXT NOT NULL, title TEXT DEFAULT NULL, unlisted BOOLEAN NOT NULL DEFAULT 0) DEFAULT CHARSET = utf8mb4');
 $db->query('CREATE TABLE IF NOT EXISTS files(id INT NOT NULL auto_increment PRIMARY KEY, time INT NOT NULL, path TEXT NOT NULL, hash TEXT DEFAULT NULL, linkid INT DEFAULT NULL) DEFAULT CHARSET = utf8mb4');
